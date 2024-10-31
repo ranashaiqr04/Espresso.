83d83758d486fc5281a443842690b000c0236e95
ranashqair04 <rhe85610@gmail.com>
2024-10-31 22:00:45 +0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+window.onscroll = function() {
+    const nav = document.querySelector(".nav-2.navbar-expand-lg "); // Use the correct class name
+    const header = document.querySelector(".header");
+
+
+    if (window.scrollY > header.offsetTop) {
+        console.log("testt");
+        nav.classList.add("scrollNavBar"); 
+    } else {
+        nav.classList.remove("scrollNavBar"); 
+    }
+};
+
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    freeMode: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+
+ 
